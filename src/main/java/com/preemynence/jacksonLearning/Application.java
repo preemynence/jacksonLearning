18ec6c832e3d135122a72b2186b021a41a229c4e
Vishal Prajapati <vishalprajapati@gmail.com>
2018-04-08 02:19:21 +0530

    moved to package.

--- src/main/java/com/preemynence/jacksonLearning/Application.java
+++ src/main/java/com/preemynence/jacksonLearning/Application.java
@@ -1,31 +1,12 @@
 package com.preemynence.jacksonLearning;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.preemynence.jacksonLearning.model.Car;
-
-import java.io.File;
-import java.io.IOException;
+import com.preemynence.jacksonLearning.examples.JSONToFileAndFileToJSON;
 
 public class Application {
 
 	public static void main(String args[]){
-		try {
-//			Create JSON file
-			ObjectMapper objectMapper = new ObjectMapper();
-			Car car = new Car("yellow", "renault");
-			objectMapper.writeValue(new File("target/car.json"), car);
-
-//			Read json from file
-			ObjectMapper objectMapper1 = new ObjectMapper();
-			Car car1 = objectMapper1.readValue(new File("target/car.json"),Car.class);
-
-//          print cars
-			System.out.print(car1);
-
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		JSONToFileAndFileToJSON jsonToFileAndFileToJSON = new JSONToFileAndFileToJSON();
+		jsonToFileAndFileToJSON.runExample();
 	}
 
 }
