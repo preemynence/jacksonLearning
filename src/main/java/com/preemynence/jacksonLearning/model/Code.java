4d3d86b975c4e4fd915e5be0ca1937b28bbf1f1b
Vishal Prajapati <vishalprajapati@gmail.com>
2018-04-17 00:01:39 +0530

    added yml

--- src/main/java/com/preemynence/jacksonLearning/model/Code.java
+++ src/main/java/com/preemynence/jacksonLearning/model/Code.java
@@ -1,9 +1,11 @@
 package com.preemynence.jacksonLearning.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Code {
 	private int[] rgba;
+	@JsonProperty(required = true)
 	private String hex;
 }
