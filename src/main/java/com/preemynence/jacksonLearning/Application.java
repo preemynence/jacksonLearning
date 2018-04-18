942e1b3951769fb9e0ab119db431d629e9c4592a
Vishal Prajapati <vishalprajapati@gmail.com>
2018-04-19 00:02:19 +0530

    Added scenario 1.

--- src/main/java/com/preemynence/jacksonLearning/Application.java
+++ src/main/java/com/preemynence/jacksonLearning/Application.java
@@ -4,6 +4,7 @@ import com.preemynence.jacksonLearning.examples.JSONToFileAndFileToJSON;
 import com.preemynence.jacksonLearning.examples.JacksonDataBindExample;
 import com.preemynence.jacksonLearning.examples.JacksonStreamExample;
 import com.preemynence.jacksonLearning.examples.JacksonTreeModelExample;
+import com.preemynence.jacksonLearning.scenarios.Scenario1;
 
 public class Application {
 
@@ -20,6 +21,9 @@ public class Application {
 		examples.runExample();
 		examples = new JacksonDataBindExample();
 		System.out.println("Running JacksonDataBindExample");
+		examples.runExample();
+		examples = new Scenario1();
+		System.out.println("Running Scenario1");
 		examples.runExample();
 	}
 
---
+++ src/main/java/com/preemynence/jacksonLearning/scenarios/Scenario1.java
@@ -0,0 +1,64 @@
+package com.preemynence.jacksonLearning.scenarios;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.preemynence.jacksonLearning.Examples;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Scenario1 implements Examples {
+	@Override
+	public void runExample() {
+		//Read the list of JSON files in to JSON Objects.
+		String file1, file2, file3, file4;
+		file1 = "target/test1.json";
+		file2 = "target/test2.json";
+		file3 = "target/test3.json";
+		file4 = "target/test4.json";
+		readJsonFilesFromLocation(file1, file2, file3, file4);
+
+		//Parse and discard the wrong JSON and process only valid JSON.
+	}
+
+	private void readJsonFilesFromLocation(String... files) {
+		for (String file : files) {
+			streamReader(file);
+		}
+	}
+
+	private void streamReader(String file) {
+		try {
+			System.out.println("Processing " + file);
+			JsonFactory jsonFactory = new JsonFactory();
+			JsonParser jParser = jsonFactory.createParser(new File(file));
+			boolean foundError = false;
+			while (jParser.nextToken() != null) {
+				if (jParser.getCurrentName() != null && jParser.getCurrentName().equals("errorCode")) {
+					foundError = true;
+					System.out.println("Found Error Code");
+					break;
+				} // else if(<any other matching condition.>)
+			}
+
+			if (foundError) {
+				System.out.println("we found error code.");
+			} else {
+				System.out.println("We did not able to find the error token");
+			}
+
+			jParser.close();
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+	}
+}
